24c8dbb4a5ac88dc2d3dc49db778097aae6b0959
Rowan Dash <pokemonmaster9967@gmail.com>
2022-11-11 15:21:46 +0000

    Create OwOify.py

---
+++ OwOify.py
@@ -0,0 +1,34 @@
+from TextToOwO.owo import text_to_owo as owo
+from os import listdir
+from os.path import isfile, join
+import os
+
+onlyfiles = [f for f in listdir("./Files") if isfile(join("./Files", f))]
+here = os.path.dirname(os.path.abspath(__file__))
+
+def owoIfy(FileName, here):
+    filename = os.path.join(here, 'Files/' + FileName)
+    output = os.path.join(here, 'OwO/' + FileName)
+    print("OwOing " + FileName)
+    #with open("Files/" + FileName + ".txt", "r", encoding='utf-8-sig') as f:
+    with open(filename, "r", encoding='utf-8-sig') as f:
+        text = f.read()
+        f.close()
+    #split file into lines
+    lines = text.splitlines()
+    
+    for i in range(len(lines)):
+        lines[i] = owo(lines[i])
+    #print(lines)
+    
+    # reassemble lines into a single text file
+    for i in range(len(lines)):
+        lines[i] = lines[i] + "\n"
+    text = "".join(lines)
+    with open(output, "w", encoding='utf-8-sig') as f:
+        f.write(text)
+        f.close()
+
+for i in range(len(onlyfiles)):
+    owoIfy(onlyfiles[i], here)
+#print(onlyfiles)
