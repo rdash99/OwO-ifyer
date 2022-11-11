807031e44ee76c8d4e84b710d624360a40a7916a
Rowan Dash <pokemonmaster9967@gmail.com>
2022-11-11 15:21:24 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from TextToOwO.owo import text_to_owo as owo
+
+print(owo("I have no mouth and I must scream"))
+# HIIII! I haz nu mouth and I must scweam XDDD
+
+print(owo("I have no mouth and I must scream"))
+# I haz nu mouth and I must scweam
